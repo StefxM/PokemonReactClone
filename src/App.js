f55721824c2c8ea848ecf3397d089ff5893cb762
stefxm <smairenax@gmail.com>
2021-06-27 16:26:34 -0400

    loading issues

--- src/App.js
+++ src/App.js
@@ -5,13 +5,36 @@ import axios from 'axios'
 
 function App() {
   const [pokemon, setPokemon ] = useState([])
-  const [currentPageUrl, setcurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
-    axios.get(currentPageUrl).then(res => {
-    setPokemon(res.data.results.map(p => p.name))
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+      }).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
   })
+
+    return () => cancel()
+
   }, [currentPageUrl])
+
+  function gotoNextPage(){
+    setCurrentPageUrl(nextPageUrl)
+  }
   
+  function gotoPrevPage(){
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  if (loading) return "Loading ..."
 
   return (
     <PokemonList pokemon={pokemon}/>
